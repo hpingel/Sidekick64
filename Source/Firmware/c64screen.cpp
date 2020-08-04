ed0b1135b2068cec6b8a458be438976612217b78
Henning Pingel <post@henningpingel.de>
2020-08-04 22:36:14 +0200

    adding missing menuItemStr param to calls of handleC64

--- Source/Firmware/c64screen.cpp
+++ Source/Firmware/c64screen.cpp
@@ -895,7 +895,7 @@ void handleC64( int k, u32 *launchKernel, char *FILENAME, char *filenameKernal, 
 		if ( k == 139 /* F6 */ )
 		{
 			menuScreen = MENU_NETWORK;
-			handleC64( 0xffffffff, launchKernel, FILENAME, filenameKernal );
+			handleC64( 0xffffffff, launchKernel, FILENAME, filenameKernal, menuItemStr );
 			return;
 		}
 #endif
@@ -1688,7 +1688,7 @@ void handleC64( int k, u32 *launchKernel, char *FILENAME, char *filenameKernal, 
 		if ( k == 136 /* F7 */ || k == 139 /* F6 */)
 		{
 			menuScreen = MENU_MAIN;
-			handleC64( 0xffffffff, launchKernel, FILENAME, filenameKernal );
+			handleC64( 0xffffffff, launchKernel, FILENAME, filenameKernal, menuItemStr );
 			return;
 		}
 		if ( k == 'x' || k == 'X')
@@ -1697,7 +1697,7 @@ void handleC64( int k, u32 *launchKernel, char *FILENAME, char *filenameKernal, 
 			{
 				pSidekickNet->redrawSktxScreen();
 				menuScreen = MENU_SKTX;
-				handleC64( 0xffffffff, launchKernel, FILENAME, filenameKernal );
+				handleC64( 0xffffffff, launchKernel, FILENAME, filenameKernal, menuItemStr );
 				return;
 			}
 		}
@@ -1728,7 +1728,7 @@ void handleC64( int k, u32 *launchKernel, char *FILENAME, char *filenameKernal, 
 		if ( k == 136 /* F7 */ || k == 139 /* F6 */)
 		{
 			menuScreen = MENU_MAIN;
-			handleC64( 0xffffffff, launchKernel, FILENAME, filenameKernal );
+			handleC64( 0xffffffff, launchKernel, FILENAME, filenameKernal, menuItemStr );
 			return;
 		}
 		/*
@@ -1774,7 +1774,7 @@ void handleC64( int k, u32 *launchKernel, char *FILENAME, char *filenameKernal, 
 		{
 			//jump to network menu from config menu - do we really need this?
 			menuScreen = MENU_NETWORK;
-			handleC64( 0xffffffff, launchKernel, FILENAME, filenameKernal );
+			handleC64( 0xffffffff, launchKernel, FILENAME, filenameKernal, menuItemStr );
 			return;
 		}		
 #endif
