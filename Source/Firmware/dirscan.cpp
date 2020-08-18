315d5a75f26e75841020bdfba2d865f0a77d49ad
Henning Pingel <post@henningpingel.de>
2020-08-18 21:29:12 +0200

    avoid constant unmounting/remounting (still has to be done in crt.cpp)

--- Source/Firmware/dirscan.cpp
+++ Source/Firmware/dirscan.cpp
@@ -551,11 +551,15 @@ void insertDirectoryContents( int node, char *basePath, int listAll )
 
 void scanDirectories( char *DRIVE )
 {
+	
+#ifndef WITH_NET
+	
 	FATFS m_FileSystem;
 
 	// mount file system
 	if ( f_mount( &m_FileSystem, DRIVE, 1 ) != FR_OK )
 		logger->Write( "RaspiMenu", LogPanic, "Cannot mount drive: %s", DRIVE );
+#endif
 
 	u32 head = 0;
 	nDirEntries = 0;
@@ -587,18 +591,24 @@ void scanDirectories( char *DRIVE )
 
 	//insertDirectoryContents( 0, "SD:" );
 
+#ifndef WITH_NET
+
 	// unmount file system
 	if ( f_mount( 0, DRIVE, 0 ) != FR_OK )
 		logger->Write( "RaspiMenu", LogPanic, "Cannot unmount drive: %s", DRIVE );
+#endif		
 }
 
 void scanDirectories264( char *DRIVE )
 {
+#ifndef WITH_NET
+	
 	FATFS m_FileSystem;
 
 	// mount file system
 	if ( f_mount( &m_FileSystem, DRIVE, 1 ) != FR_OK )
 		logger->Write( "RaspiMenu", LogPanic, "Cannot mount drive: %s", DRIVE );
+#endif
 
 	u32 head = 0;
 	nDirEntries = 0;
@@ -606,8 +616,9 @@ void scanDirectories264( char *DRIVE )
 	APPEND_SUBTREE_UNSCANNED( "D264", "SD:D264", 0 )
 	APPEND_SUBTREE_UNSCANNED( "PRG264", "SD:PRG264", 0 )
 
+#ifndef WITH_NET
 	// unmount file system
 	if ( f_mount( 0, DRIVE, 0 ) != FR_OK )
 		logger->Write( "RaspiMenu", LogPanic, "Cannot unmount drive: %s", DRIVE );
+#endif
 }
-
