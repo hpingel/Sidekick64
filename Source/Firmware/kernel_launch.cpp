ab3ba20b7009d9d0b0666e4b10ba64102f2641e3
Henning Pingel <post@henningpingel.de>
2020-09-21 23:58:49 +0200

    allow net triggered reboot from launch mode

--- Source/Firmware/kernel_launch.cpp
+++ Source/Firmware/kernel_launch.cpp
@@ -227,7 +227,7 @@ void CKernelLaunch::Run( void )
 	nBytesRead = 0; stage = 1;
 	u32 cycleCountC64_Stage1 = 0;
 	#ifdef WITH_NET		
-	unsigned netDelay = 90000000;
+	unsigned netDelay = 90000000; //TODO: improve this
 	#endif
 
 	// wait forever
@@ -277,6 +277,10 @@ void CKernelLaunch::Run( void )
 				m_InputPin.DisableInterrupt();
 				m_InputPin.DisconnectInterrupt();
 				EnableIRQs();
+				
+				if ( pSidekickNet->isReturnToMenuRequired())
+					return;
+				
 				kernelMenu->updateSystemMonitor();
 				pSidekickNet->handleQueuedNetworkAction();
 				DisableIRQs();
