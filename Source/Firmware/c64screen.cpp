f5266f7723b7ede6e7b94c9953b49e44e6a2ddd7
hpingel <post@henningpingel.de>
2021-01-12 00:18:18 +0100

    unfinished refactoring for nmi screen refresh in kernel_menu

--- Source/Firmware/c64screen.cpp
+++ Source/Firmware/c64screen.cpp
@@ -1708,6 +1708,7 @@ void handleC64( int k, u32 *launchKernel, char *FILENAME, char *filenameKernal, 
 		{
 			//if (pSidekickNet->IsRunning())
 			{
+				pSidekickNet->enteringSktpScreen();
 				menuScreen = MENU_SYSTEMINFO;
 				handleC64( 0xffffffff, launchKernel, FILENAME, filenameKernal, menuItemStr );
 				return;
@@ -1781,6 +1782,7 @@ void handleC64( int k, u32 *launchKernel, char *FILENAME, char *filenameKernal, 
 	{
 		if ( k == VK_F7)
 		{
+			pSidekickNet->leavingSktpScreen();
 			menuScreen = MENU_MAIN;
 			handleC64( 0xffffffff, launchKernel, FILENAME, filenameKernal, menuItemStr );
 			return;
