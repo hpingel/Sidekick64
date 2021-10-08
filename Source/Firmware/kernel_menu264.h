82a8d333a86f2d83cb2d8c13c16985c34a51f6dd
hpingel <post@henningpingel.de>
2021-10-08 23:42:39 +0200

    sk264 network updates ported from sk64 code

--- Source/Firmware/kernel_menu264.h
+++ Source/Firmware/kernel_menu264.h
@@ -131,7 +131,8 @@ public:
 
 private:
 	static void FIQHandler( void *pParam );
-	void DisableFIQInterrupt( void );
+	void enableFIQInterrupt();
+	void DisableFIQInterrupt();
 #ifdef WITH_NET
 	boolean handleNetwork( boolean);
 #endif
