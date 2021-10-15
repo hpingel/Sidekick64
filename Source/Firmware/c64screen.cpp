35ad98e893a51058503ed32841ba6e540903ae12
hpingel <post@henningpingel.de>
2021-10-15 19:39:03 +0200

    sktp charset mapping changed (now just petscii 2 screencode)

--- Source/Firmware/c64screen.cpp
+++ Source/Firmware/c64screen.cpp
@@ -2397,7 +2397,7 @@ void printSKTPScreen()
 				content = (char *) pSidekickNet->GetSktpScreenContentChunk( pos, color, inverse);
 				y = pos / 40;
 				x = pos % 40;
-				printC64( x, y+yOffset, content, color, inverse ? 0x80 : 0, 4);//4 is undefined
+				printC64( x, y+yOffset, content, color, inverse ? 0x80 : 0, 1);
 			}
 			pSidekickNet->ResetSktpScreenContentChunks();
 		}
