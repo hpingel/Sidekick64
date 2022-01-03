7633a0cd72b0f8d90cf05d1811ee190e95abc43f
hpingel <post@henningpingel.de>
2022-01-04 00:16:51 +0100

    remove red paint on s keypress

--- C64Side/rpimenu.c
+++ C64Side/rpimenu.c
@@ -302,6 +302,7 @@ int main (void)
         // sendKeypress
         //key = cgetc();
 				//key == 29 ||
+				/*
         if ( ( key == 's' || key == 'S' ) && *((char *)(0x0427)) != 0 )				
 				{
 						__asm__ ("lda #$0a");
@@ -310,7 +311,7 @@ int main (void)
 						__asm__ ("sta $d850,x");
 						__asm__ ("dex");
 						__asm__ ("bne loop");
-				}
+				}*/
 
 				        wireDetection();
 				        *((char *)(0xdf01)) = key;
