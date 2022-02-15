fbf726b8b039eab36e31015a03c5eec06f3fa38c
hpingel <post@henningpingel.de>
2022-02-15 22:54:57 +0100

    fixed 264 make

--- Source/Firmware/kernel_sid264.cpp
+++ Source/Firmware/kernel_sid264.cpp
@@ -253,7 +253,6 @@ unsigned long long cycleCountC64;
 #define SAMPLERATE 44100
 u32 SAMPLERATE_ADJUSTED = SAMPLERATE;
 u32 trackSampleProgress;
-u32 fillSoundBuffer;
 
 #ifdef COMPILE_MENU
 extern CLogger			*logger;
