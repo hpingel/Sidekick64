00614dd5947943f1a96bde45bac45b6e133a8cb7
hpingel <post@henningpingel.de>
2022-02-19 23:00:33 +0100

    fix vanilla make

--- Source/Firmware/kernel_menu.cpp
+++ Source/Firmware/kernel_menu.cpp
@@ -1208,7 +1208,11 @@ void CKernelMenu::Run( void )
 
 			convertScreenToBitmap( framebuffer );
 
+#ifdef WITH_NET
 			if ( showAnimation && currentVDCMode < 2 && !m_SidekickNet.isSKTPScreenActive() )
+#else
+			if ( showAnimation && currentVDCMode < 2 )
+#endif
 			{
 				ctn++;
 				if ( currentVDCMode == 1 )
@@ -1637,7 +1641,11 @@ void CKernelMenu::Run( void )
 			nBytesToTransfer = 7 * 8 * 64;
 
 			// transfer animation only if VIC-output is active
+#ifdef WITH_NET			
 			if ( currentVDCMode < 2 && !m_SidekickNet.isSKTPScreenActive())
+#else			
+			if ( currentVDCMode < 2)
+#endif			
 			while ( nBytesToTransfer > 0 ) 
 			{
 				nBytesToTransfer --;
